267cdb6c14394899440333bfcbba65f5d38ea1f1
lara-ashley <lara.goncalves14@aluno.ce.gov.br>
2025-12-08 14:55:52 -0300

    add GenreController

---
+++ Documents/projetos/ReviewCine/app/Http/Controllers/GenreController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Genre;
+use Illuminate\Http\Request;
+
+class GenreController extends Controller
+{
+    public function index()
+    {
+        $genres = Genre::all();
+        return view('genres.index', compact('genres'));
+    }
+
+    public function create()
+    {
+        return view('genres.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|min:2',
+            'description' => 'nullable',
+        ]);
+
+        Genre::create($request->all());
+
+        return redirect()->route('genres.index')
+            ->with('success', 'Gênero criado com sucesso!');
+    }
+
+    public function edit(Genre $genre)
+    {
+        return view('genres.edit', compact('genre'));
+    }
+
+    public function update(Request $request, Genre $genre)
+    {
+        $request->validate([
+            'name' => 'required|min:2',
+            'description' => 'nullable',
+        ]);
+
+        $genre->update($request->all());
+
+        return redirect()->route('genres.index')
+            ->with('success', 'Gênero atualizado com sucesso!');
+    }
+
+    public function destroy(Genre $genre)
+    {
+        $genre->delete();
+
+        return redirect()->route('genres.index')
+            ->with('success', 'Gênero deletado!');
+    }
+}
